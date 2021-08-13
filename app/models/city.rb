e0eec6766dde2f203e113715b881b25fd6c085d8
minirachel <rachel.a.levinson@gmail.com>
2021-08-13 16:59:35 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,30 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(date1, date2)
+    # listings available for every date between 1 and 2 including 1 and 2
+    date01 = date1.to_date
+    date02 = date2.to_date
 
+    date_range = []
+
+    date01.upto(date02) do |date|
+       date_range << date
+    end
+
+
+    available_listings = self.listings.select do |l|
+      date_range.each do |date|
+        l.reservations.select do |r| 
+          binding.pry
+          r.status == "available" && r.checkin != date && r.checkout != date
+        end
+      end
+    end
+
+    available_listings
+
+  end
+end
+#need to compare search_date_range with reservation_date_range
+# rspec ./spec/models/city_spec.rb:13
