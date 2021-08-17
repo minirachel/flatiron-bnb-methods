10f0ba768a01b2fd8ecb205da9569c65f57a749c
minirachel <rachel.a.levinson@gmail.com>
2021-08-17 13:29:03 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,27 +2,28 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-  def city_openings(date1, date2)
-    # listings available for every date between 1 and 2 including 1 and 2
-    search_date_start = date1.to_date
-    search_date_end = date2.to_date
+#   def city_openings(start_date, end_date)
 
-    # date_range = []
+#     # self.listings.merge(Listing.available(start_date, end_date))
 
-    # search_date_start.upto(search_date_end) do |date|
-    #    date_range << date
-    # end
+#     # listings available for every date between 1 and 2 including 1 and 2
+#     # search_date_start = date1.to_date
+#     # search_date_end = date2.to_date
 
+#     # available_listings = []
+#     # unavailable_listings = []
 
-    self.listings.select do |l|
-      l.reservations.select do |r| 
-        res_date_start = r.checkin
-        res_date_end = r.checkout
-        r.status == "available" && (search_date_start <= res_date_end) && (search_date_end >= res_date_start)
-      end
-    end
+#     # self.listings.each do |l|
+#     #   l.reservations.each do |r| 
+#     #     res_date_start = r.checkin
+#     #     res_date_end = r.checkout
+#     #     (search_date_start <= res_date_end) && (search_date_end >= res_date_start)
+#     #   end
+#     # end
 
-  end
+#     # available_listings
+# end
+
 end
-#need to compare search_date_range with reservation_date_range
-# rspec ./spec/models/city_spec.rb:13
+
+
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,38 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood, presence: true
+
+  after_save :change_user_to_host
+  before_destroy :remove_host
+
+
+  private
+
+  def change_user_to_host
+    host.update(host: true)
+  end
+
+  def remove_host
+    if Listing.where(host: host).where.not(id: id).empty?
+      host.update(host: false)
+    end
+  end
+
+  # def self.available(start_date, end_date)
+  #   if start_date && end_date
+  #     joins(:reservations).
+  #       where.not(reservations: {check_in: start_date..end_date}) &
+  #     joins(:reservations).
+  #       where.not(reservations: {check_out: start_date..end_date})
+  #   else
+  #     []
+  #   end
+  # end
 end
