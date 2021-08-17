10898fe3df3f4caccaab06a761d54e2d5e63877f
minirachel <rachel.a.levinson@gmail.com>
2021-08-17 11:39:39 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,26 +4,23 @@ class City < ActiveRecord::Base
 
   def city_openings(date1, date2)
     # listings available for every date between 1 and 2 including 1 and 2
-    date01 = date1.to_date
-    date02 = date2.to_date
+    search_date_start = date1.to_date
+    search_date_end = date2.to_date
 
-    date_range = []
+    # date_range = []
 
-    date01.upto(date02) do |date|
-       date_range << date
-    end
+    # search_date_start.upto(search_date_end) do |date|
+    #    date_range << date
+    # end
 
 
-    available_listings = self.listings.select do |l|
-      date_range.each do |date|
-        l.reservations.select do |r| 
-          binding.pry
-          r.status == "available" && r.checkin != date && r.checkout != date
-        end
+    self.listings.select do |l|
+      l.reservations. do |r| 
+        res_date_start = r.checkin
+        res_date_end = r.checkout
+        r.status == "available" && (search_date_start <= res_date_end) && (search_date_end >= res_date_start)
       end
     end
-
-    available_listings
 
   end
 end
